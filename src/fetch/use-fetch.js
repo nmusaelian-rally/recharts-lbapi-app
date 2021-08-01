89259347ff66752d6a8b6c0eb62195de3e7f7a18
nmusaelian-rally <nik.musaelian@broadcom.com>
2021-07-31 20:26:13 -0600

    axios.all takes dynamically created array

--- src/fetch/use-fetch.js
+++ src/fetch/use-fetch.js
@@ -3,24 +3,31 @@ import { useState, useCallback } from 'react';
 import axios from 'axios';
 
 const useFetch = () => {
-  //const [data, setData] = useState(initState);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
   const makeRequest = useCallback(async (requestCfg, packageData) => {
       setLoading(true);
       setError(null);
-      const [url1, url2] = requestCfg.urls;
+      //const [url1, url2] = requestCfg.urls;
       const options = {
         method: requestCfg.method ? requestCfg.method : 'GET',
         headers: requestCfg.headers ? requestCfg.headers : {},
         body: requestCfg.body ? JSON.stringify(requestCfg.body) : null
       }
+      const axiosAll = [];
+      for(const url of requestCfg.urls){
+          let newPromise = axios({
+              url: url,
+              method: options.method,
+              headers: options.headers,
+              body: options.body,
+          })
+          axiosAll.push(newPromise)
+      }
+      console.log(axiosAll)
       try{
-        const result = await axios.all([
-            axios.get(url1, options),
-            axios.get(url2, options)
-          ]);
+        const result = await axios.all(axiosAll);
         packageData({
             inProgress: result[0].data['Results'],
             released:   result[1].data['Results']
